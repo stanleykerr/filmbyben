8bf350167c4db95a05d4870c9c4d4252eefba81c
stanleykerr <stanleyfkerr@gmail.com>
2022-03-30 06:25:12 -0700

    Leaving babel in the dust for SWC rust compiler

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["jsx", "js", "ts", "tsx"],
   images: {
     domains: ["uploads-ssl.webflow.com", "img.youtube.com"],
   },
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
